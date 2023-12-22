c760b84ca754eb3a62eb8ea16f0e8a3ed2870c42
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2023-12-22 11:10:35 -0500

    Using metadata Fashion-MNIST dataset for benchmarking machine learning algorithms

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ import numpy as np
 import tensorflow_datasets as tfds 
 import matplotlib.pyplot as plt 
 import math
+
+
+""" Fashion-MNIST is a dataset of Zalando's 
+    article images — consisting of a training set 
+    of 60,000 examples and a test set of 10,000 examples 
+    for benchmarking machine learning algorithms.
+"""
+
+data, metadata = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
